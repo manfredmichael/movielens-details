0bdd4a2d5be2b2bc71df98c2e28e0a1958bc8023
manfredmichael <koskosanrider@gmail.com>
2021-10-12 23:33:34 +0700

    save scrapping all scrapping result

--- main.py
+++ main.py
@@ -9,15 +9,13 @@ def main():
     movielens_df = pd.read_csv('movielens_poster.csv')
 
     movielens = movielens_df.to_dict('records')
-    for i, row in enumerate(movielens[:10]):
+    for i, row in tqdm(enumerate(movielens)):
         result = page.get_details(row['movie_url'])
         row['description'] = result['description']
         movielens[i] = row
-    print(movielens[:12])
 
-    # movielens['description'] = movielens['movie_url'].progress_apply(lambda x: page.get_description(x))
-    # movielens.to_csv('movielens100k_details.csv', index=False)
-    # movielens.to_json('movielens100k_details.json', index=False)
+    movielens.to_csv('movielens100k_details.csv', index=False)
+    movielens.to_json('movielens100k_details.json', index=False)
         
 
 if __name__=='__main__':
