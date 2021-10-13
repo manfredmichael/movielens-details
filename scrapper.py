5906e8a41e1bee1c2cb7f2cd167193e714a3f388
manfredmichael <koskosanrider@gmail.com>
2021-10-13 15:06:16 +0700

    scrape director and writer

--- scrapper.py
+++ scrapper.py
@@ -20,16 +20,15 @@ class IMDBPage:
         self.browser = browser
 
     def get_details(self, movie_url):
-        self.browser.get(movie_url)
-
+        self.load_page(movie_url)
         age_rating, year, movie_length = self.get_info()
-        director, writer = self.get_crew()
-        rating, reviewer = self.get_rating()
+        director, writer = self.get_cast()
+        rating, num_review = self.get_rating()
         description = self.get_description(movie_url)
 
         return {'description': description,
                 'rating': rating,
-                'reviewer': reviewer,
+                'num_review': num_review,
                 'director': director,
                 'writer': writer,
                 'age_rating': age_rating,
@@ -46,23 +45,29 @@ class IMDBPage:
         movie_length = info[2].text
         return age_rating, year, movie_length
 
-    def get_crew(self):
-        crew = WebDriverWait(self.browser, 3).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='PrincipalCredits__PrincipalCreditsPanelWideScreen-hdn81t-0 iGxbgr']"))
-        ).text
+    def get_cast(self):
+        try:
+            cast_box = self.browser.find_element_by_xpath("//section[@data-testid='title-cast']")
+        except NoSuchElementException:
+            cast_box = None
+        
+        director_box, writer_box = tuple(cast_box.find_element_by_xpath("//ul[@class='ipc-metadata-list ipc-metadata-list--dividers-all StyledComponents__CastMetaDataList-y9ygcu-10 cbPPkN ipc-metadata-list--base']") \
+                                                 .find_elements_by_xpath("//li[@class='ipc-metadata-list__item']")[:2])
 
-        director = crew.split('\n')[1]
-        writer = crew.split('\n')[3]
-        return director, writer 
+        directors = director_box.find_elements_by_tag_name('li')
+        directors = [director.find_element_by_tag_name('a').text for director in directors] 
+
+        writers = writer_box.find_elements_by_tag_name('li')
+        writers = [writer.find_element_by_tag_name('a').text for writer in writers] 
+
+        return directors, writers
 
     def get_rating(self):
-        rating_box = WebDriverWait(self.browser, 20).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='AggregateRatingButton__ContentWrap-sc-1ll29m0-0 hmJkIS']"))
-        ).text
+        rating_box = self.browser.find_element_by_xpath("//div[@class='AggregateRatingButton__ContentWrap-sc-1ll29m0-0 hmJkIS']").text
         
         rating = rating_box.split('\n')[0].strip() 
-        reviewer = rating_box.split('\n')[-1].strip() 
-        return rating, reviewer 
+        num_review= rating_box.split('\n')[-1].strip() 
+        return rating, num_review 
 
     def get_description(self, movie_url):
         description_url = movie_url.replace('?ref_=', 'plotsummary?ref_=')
@@ -73,3 +78,9 @@ class IMDBPage:
         )
         description = description_box.find_element_by_tag_name('p')
         return description.text.strip()
+
+    def load_page(self, movie_url):
+        self.browser.get(movie_url)
+        WebDriverWait(self.browser, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class='ipc-poster ipc-poster--baseAlt ipc-poster--dynamic-width Poster__CelPoster-sc-6zpm25-0 kPdBKI celwidget ipc-sub-grid-item ipc-sub-grid-item--span-2']"))
+        )
