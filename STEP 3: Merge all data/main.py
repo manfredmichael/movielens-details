7e0451406686beb25e07171525e8a60f22f44722
manfredmichael <koskosanrider@gmail.com>
2021-10-16 20:13:32 +0700

    fix parsing string to list for writers, directors, and casts

--- STEP 3: Merge all data/main.py
+++ STEP 3: Merge all data/main.py
@@ -10,38 +10,29 @@ with open('genre.json') as f:
 full_csv = pd.DataFrame()
 full_json = dict()
 
-# merge all csv batches
-for filename in os.listdir('csv'):
-    filepath = os.path.join('csv', filename)
-    batch_csv = pd.read_csv(filepath, index_col=0)
-    full_csv = full_csv.append(batch_csv)
-
-# add genre in string format
-full_csv['movie_genres'] = full_csv['movie_genres'].apply(
-    lambda genres: [int(i) for i in genres.replace('[','').replace(']','').split()]
-)
-full_csv['genre'] = full_csv['movie_genres'].apply(
-    lambda genres: [genre_to_str[str(genre)] for genre in genres if genre in genre_to_str.keys()]
-)
-
-full_csv.to_csv('../movielens100k_details.csv', index=False)
-
-
-
-full_csv = pd.DataFrame()
-
 # merge all batches
 for filename in os.listdir('csv'):
     filepath = os.path.join('csv', filename)
     batch_csv = pd.read_csv(filepath, index_col=0)
     full_csv = full_csv.append(batch_csv)
 
-# add genre in string format
+# parse string to list
 full_csv['movie_genres'] = full_csv['movie_genres'].apply(
-    lambda genres: [int(i) for i in genres.replace('[','').replace(']','').split()]
+    lambda x: [int(i) for i in x.replace('[','').replace(']','').split()]
 )
+full_csv['director'] = full_csv['director'].apply(
+    lambda x: [str(i).replace('\'','').replace('\"','').strip() for i in str(x).replace('[','').replace(']','').split(',')]
+)
+full_csv['writer'] = full_csv['writer'].apply(
+    lambda x: [str(i).replace('\'','').replace('\"','').strip() for i in str(x).replace('[','').replace(']','').split(',')]
+)
+full_csv['cast'] = full_csv['cast'].apply(
+    lambda x: [str(i).replace('\'','').replace('\"','').strip() for i in str(x).replace('[','').replace(']','').split(',')]
+)
+
+# add genre in string format
 full_csv['genre'] = full_csv['movie_genres'].apply(
-    lambda genres: [genre_to_str[str(genre)] for genre in genres if str(genre) in genre_to_str.keys()]
+    lambda genres: [genre_to_str[str(genre)] for genre in genres if genre in genre_to_str.keys()]
 )
 
 # save as csv
